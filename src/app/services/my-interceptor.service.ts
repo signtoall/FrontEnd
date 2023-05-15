48202298a44987b29b61b9cfa5155e1b810290f6
staradrik <adrianayara12@gmail.com>
2023-05-15 15:50:46 -0500

    interceptors http

--- src/app/services/my-interceptor.service.ts
+++ src/app/services/my-interceptor.service.ts
@@ -1,18 +1,41 @@
 import { Injectable } from '@angular/core';
-import { 
-    HttpInterceptor, 
-    HttpHandler, 
-    HttpRequest, 
-    HttpEvent 
-} from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest,HttpErrorResponse } from '@angular/common/http';
+import { catchError, Observable, throwError } from 'rxjs';
+
+import { Router } from '@angular/router';
 
 @Injectable()
 export class MyInterceptor implements HttpInterceptor {
-    intercept(req : HttpRequest<any>, next : HttpHandler) : Observable<HttpEvent<any>> {
-      const httpReq = req.clone({
-        url: req.url.replace("http://", "https://")
+   
+  constructor(private router: Router) { }
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+
+    
+    const token: string = localStorage.getItem('token');
+
+    let request = req;
+
+    if (token) {
+      request = req.clone({
+        setHeaders: {
+          authorization: `Bearer ${ token }`
+        }
       });
-      return next.handle(httpReq);
     }
+
+    //return next.handle(request);
+
+    return next.handle(request).pipe(
+      catchError((err: HttpErrorResponse) => {
+
+        if (err.status === 401) {
+          this.router.navigateByUrl('');
+        }
+
+        return throwError( err );
+
+      })
+    );
+  }
 }
